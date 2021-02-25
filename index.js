9e23d0092222eea9a84d868352a193012b90f35f
Chad Courtney <Chadcourtney567@gmail.com>
2021-02-25 17:31:54 -0500

    went ham on style, index.html temp

--- index.js
+++ index.js
@@ -8,7 +8,85 @@ const inquirer = require("inquirer");
 teamInit = () => {
     return inquirer.prompt([
         {
-
+            type: "input",
+            name: "manName",
+            message: "Enter name here"
+        },
+        {
+            type: "input",
+            name: "manID",
+            message: "Enter manager ID"
+        },
+        {
+            type: "input",
+            name: "manEmail",
+            message: "Enter managers Email"
+        },
+        {
+            type: "input",
+            name: "officeNum",
+            message: "Enter manager office number"
         }
     ])
-}
+};
+
+nextMemb = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "nextMembChoice",
+            message: "Add another type of employee",
+            choices: ["Manager", "Engineer", "Intern", "No more employees!"]
+        }
+    ])
+};
+
+engineerPrompt = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "enginName",
+            message: "Enter name here"
+        },
+        {
+            type: "input",
+            name: "enginID",
+            message: "Enter engineer ID"
+        },
+        {
+            type: "input",
+            name: "enginEmail",
+            message: "Enter engineers Email"
+        },
+        {
+            type: "input",
+            name: "gitHub",
+            message: "Enter engineer GitHub username"
+        }
+    ])
+};
+
+internPrompt = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "internName",
+            message: "Enter name here"
+        },
+        {
+            type: "input",
+            name: "internID",
+            message: "Enter intern ID"
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "Enter interns Email"
+        },
+        {
+            type: "input",
+            name: "gitHub",
+            message: "Enter intern School name"
+        }
+    ])
+};
