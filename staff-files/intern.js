64559e2332fa38ed336f5b0cc0bf94813b904ee1
Chad Courtney <Chadcourtney567@gmail.com>
2021-02-25 14:44:45 -0500

    forgot to add intern save

--- staff-files/intern.js
+++ staff-files/intern.js
@@ -1,5 +1,9 @@
 const inquirer = require("inquirer");
 const Employee = require("./employee");
 
-class Intern extends Employee
+class Intern extends Employee {
+    constructor() {
+
+    }
+}
 module.exports = Intern;
